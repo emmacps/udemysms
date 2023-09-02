38a4f2cf6ace34d90272cfcc33a6e4e6a6ac638e
emmacps <emmacps001@gmail.com>
2023-09-02 17:26:24 +0000

    manage profile

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,5 +45,17 @@ Route::prefix('users')->group(function(){
     Route::get('/edit/{id}', [UserController::class, 'UserEdit'])->name('users.edit');
     Route::post('/update/{id}', [UserController::class, 'UserUpdate'])->name('users.update');
     Route::get('/delete/{id}', [UserController::class, 'UserDelete'])->name('users.delete');
+
+});
+
+// user profile management all route
+Route::prefix('profile')->group(function(){
+
+    Route::get('/view', [ProfileController::class, 'ProfileView'])->name('profile.view');
+    Route::get('/edit', [ProfileController::class, 'ProfileEdit'])->name('profile.edit');
+    Route::post('/store', [ProfileController::class, 'ProfileStore'])->name('profile.store');
+    Route::get('/password/view', [ProfileController::class, 'PasswordView'])->name('password.view');
+    Route::post('/password/update', [ProfileController::class, 'PasswordUpdate'])->name('password.update');
+   // Route::get('/delete/{id}', [UserController::class, 'UserDelete'])->name('users.delete');
 
 });
